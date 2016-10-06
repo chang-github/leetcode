ed603a6e7a2a68584040653ba93263a71be7fcab
chang-github <wenchangm@yahoo.com>
2016-10-06 16:03:30 -0400

    delete some useless code

--- 7_reverse integer2.java
+++ 7_reverse integer2.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int reverse(int x) {
-        if(x<Integer.MAX_VALUE&&x>Integer.MIN_VALUE){
+     //   if(x<Integer.MAX_VALUE&&x>Integer.MIN_VALUE){
             boolean f = true;
             long r = 0;
             if(x<0){
@@ -10,11 +10,11 @@ public class Solution {
             while(x>0){
                 r = r*10+x%10;
                 x=x/10;
-                if(r>Integer.MAX_VALUE||r<Integer.MIN_VALUE) return 0;
+                if(r>Integer.MAX_VALUE) return 0;
             }
             if(f) return (int)(r);
             else return (int)(-1*r);
-        }
-        return 0;
+       // }
+      //  return 0;
     }
 }
