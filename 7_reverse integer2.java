36a57ca71f6a245e60a7be0b277ebd014acb8cad
chang-github <wenchangm@yahoo.com>
2016-10-06 15:51:03 -0400

    #7 solution smaller program

---
+++ 7_reverse integer2.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int reverse(int x) {
+        if(x<Integer.MAX_VALUE&&x>Integer.MIN_VALUE){
+            boolean f = true;
+            long r = 0;
+            if(x<0){
+                x = -1*x;
+                f = false;
+            }
+            while(x>0){
+                r = r*10+x%10;
+                x=x/10;
+                if(r>Integer.MAX_VALUE||r<Integer.MIN_VALUE) return 0;
+            }
+            if(f) return (int)(r);
+            else return (int)(-1*r);
+        }
+        return 0;
+    }
+}
