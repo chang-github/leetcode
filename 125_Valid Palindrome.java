e8dd7d4f480c52144e8c4ac3183d9bcd924e5964
chang-github <wenchangm@yahoo.com>
2016-10-07 15:14:32 -0400

    125 solution

---
+++ 125_Valid Palindrome.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length()==0) return true;
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            while(!Character.isLetterOrDigit(s.charAt(i))&&i<j) i++;
+            while(!Character.isLetterOrDigit(s.charAt(j))&&i<j) j--;
+            if(Character.toLowerCase(s.charAt(i))==Character.toLowerCase(s.charAt(j))){
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
